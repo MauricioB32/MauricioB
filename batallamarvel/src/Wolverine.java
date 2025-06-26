c5476442502fea717ae02b70c9d5d8b0ad82c723
Mauricio Bustamante <3116009101b62@gmail.com>
2025-06-25 23:06:10 -0900

    Add files via upload

---
+++ batallamarvel/src/Wolverine.java
@@ -0,0 +1,7 @@
+public class Wolverine extends Personaje {
+    
+    public Wolverine(int vidaInicial) {
+        // Llama al constructor de la clase base con los valores de Wolverine
+        super("Wolverine", vidaInicial, 10, 120, 0.20);
+    }
+}
