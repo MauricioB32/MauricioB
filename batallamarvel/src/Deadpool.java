1c336234b14237d6ac2561f482aa254a948a48db
Mauricio Bustamante <3116009101b62@gmail.com>
2025-08-24 23:48:13 -0500

    Delete batallamarvel directory

--- batallamarvel/src/Deadpool.java
+++
@@ -1,7 +0,0 @@
-public class Deadpool extends Personaje {
-    
-    public Deadpool(int vidaInicial) {
-        // Llama al constructor de la clase base con los valores de Deadpool
-        super("Deadpool", vidaInicial, 10, 100, 0.25);
-    }
-}
--- batallamarvel/src/Wolverine.java
+++
@@ -1,7 +0,0 @@
-public class Wolverine extends Personaje {
-    
-    public Wolverine(int vidaInicial) {
-        // Llama al constructor de la clase base con los valores de Wolverine
-        super("Wolverine", vidaInicial, 10, 120, 0.20);
-    }
-}
